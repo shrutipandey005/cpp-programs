6fa1aeb7d9357085fd1f41f26252fabc61d22444
shrutipandey005 <pandeyshruti126@gmail.com>
2026-07-21 23:01:48 +0530

    Added fibonacci program

---
+++ fibonacci.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a = 0, b = 1;
+    cout << a << " " << b << " ";
+
+    for (int i = 2; i < n; i++) {
+        int c = a + b;
+        cout << c << " ";
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
