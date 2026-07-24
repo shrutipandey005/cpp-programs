26c5b8dfba06850b806e5116393c488e5e4110ff
shrutipandey005 <pandeyshruti126@gmail.com>
2026-07-24 20:32:50 +0530

    Added compare strings program

---
+++ compare_strings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+
+    if(a == b)
+        cout << "Equal";
+    else
+        cout << "Not Equal";
+
+    return 0;
+}
