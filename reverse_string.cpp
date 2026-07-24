d4df4a11547bccb5ef89488c32b10b92c2d338a1
shrutipandey005 <pandeyshruti126@gmail.com>
2026-07-24 20:29:53 +0530

    Added reverse string program

---
+++ reverse_string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+
+    int n = str.length();
+
+    for(int i = n - 1; i >= 0; i--) {
+        cout << str[i];
+    }
+
+    return 0;
+}
