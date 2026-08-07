1149921e674521bc803095085690f5db470b9cc8
shrutipandey005 <pandeyshruti126@gmail.com>
2026-08-07 23:00:38 +0530

    Added anagram check program

---
+++ anagram.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string s1, s2;
+
+    cout << "Enter first string: ";
+    cin >> s1;
+    cout << "Enter second string: ";
+    cin >> s2;
+
+    if (s1.length() != s2.length()) {
+        cout << "Not Anagram";
+        return 0;
+    }
+
+    sort(s1.begin(), s1.end());
+    sort(s2.begin(), s2.end());
+
+    if (s1 == s2)
+        cout << "Anagram";
+    else
+        cout << "Not Anagram";
+
+    return 0;
+}
